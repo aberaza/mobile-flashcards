b714bc8ce326f683c99eee2247e3e6021076fd2b
Aritz Beraza <aritz.beraza@gmail.com>
2018-02-03 21:22:28 +0100

    fix:removed unwanted logs

--- utils/api.js
+++ utils/api.js
@@ -8,19 +8,14 @@ export function fetchDecks() {
 }
 
 export function submitDeck(title){
-    console.log("submit deck " + title)
     return AsyncStorage.mergeItem(DECKS_KEY, JSON.stringify({[title]:{title,questions:[]}}))
-    //   .then(console.log)
 }
 
 export function addQuestion(deck, question) {
-    console.log("add card to deck " + deck, question)
     return AsyncStorage.getItem(DECKS_KEY)
         .then((results) => {
             let data = JSON.parse(results)
-            console.log("api ",data)
             data[deck].questions.push(question)
-            console.log("api ", data)
             AsyncStorage.setItem(DECKS_KEY, JSON.stringify(data))
         }
     )
